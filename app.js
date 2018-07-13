5074432495c55867cc549a19f78a2c0acf396e1f
LeeVaughn <dlvprogramming@gmail.com>
2018-07-12 19:36:34 -0700

    Added new dependencies

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 const Twit = require("twit");
+const config = require("./config.js");
+const T = new Twit(config);
+
+// tells express which template engine to use
+app.set("view engine", "pug");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/static", express.static("./public"));
+
+// creates server
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000.');
+  });
