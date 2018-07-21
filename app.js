f6793c1170d360f14b074be71f63ac7fb63bd4a1
LeeVaughn <dlvprogramming@gmail.com>
2018-07-20 20:27:05 -0700

    formated file and added some notes

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const T = new Twit(config);
 // tells express which template engine to use
 app.set("view engine", "pug");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use("/static", express.static("./public"));
 
-// creates server
+// creates server running on localhost:3000
 app.listen(3000, () => {
-    console.log('The application is running on localhost:3000.');
-  });
+  console.log('The application is running on localhost:3000!');
+});
