1647c13b082f2785970032ecf456c438d5b2c50c
LeeVaughn <dlvprogramming@gmail.com>
2018-07-21 13:17:45 -0700

    added function to retrieve user data

--- app.js
+++ app.js
@@ -4,14 +4,32 @@ const bodyParser = require("body-parser");
 const Twit = require("twit");
 const config = require("./config.js");
 const T = new Twit(config);
+const user = {};
 
 // tells express which template engine to use
 app.set("view engine", "pug");
 
+// tells express to use bodyParser and where static files are located
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use("/static", express.static("./public"));
+
+// uses Twit to retrieve user data
+T.get("account/verify_credentials", (err, data, res, next) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+  // adds user data to user object
+  user.id = data.id;
+  user.name = data.name;
+  user.screenName = data.screen_name;
+  user.friendsCount = data.friends_count;
+  user.profileImage = data.profile_image_url;
+  user.profileBanner = data.profile_banner_url;
+  console.log(user);
+});
 
 // creates server running on localhost:3000
 app.listen(3000, () => {
